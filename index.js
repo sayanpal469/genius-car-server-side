d37dd18ff6eb19bce37f29de903cf8b0fa70f91b
sayanpal469 <sayanpal469@gmail.com>
2022-05-01 19:24:11 +0530

    'first-commit'

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+
+// middlewar
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sfuz2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect()
+        const serviceCollection = client.db('geniusCar').collection('service')
+        const orderCollection = client.db('geniusCar').collection('order')
+
+
+
+        // Login JWT
+        app.post('/login', (req, res) => {
+            const email = req.body
+
+            const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+            res.send({token})
+        })
+
+        app.get('/service', async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+
+            res.send(services)
+        })
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+
+        app.post('/service', async (req, res) => {
+            const newService = req.body
+
+            const tokenInfo = req.headers.authorization
+            const [email, accessToken] = tokenInfo.split(" ")
+            const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+            console.log(decoded);
+
+            if(email === decoded.email) {
+              const result = await serviceCollection.insertOne(newService)
+              res.send(result)
+            } else{
+                res.send({success: 'unAccessToken'})
+            }
+
+        })
+
+        // Order
+
+        app.get('/order', async (req, res) => {
+            const email = req.query.email
+            const query = {email: email}
+            const cursor = orderCollection.find(query)
+            const orders = await cursor.toArray()
+
+            res.send(orders)
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body
+            const result = await orderCollection.insertOne(order)
+            res.send(result)
+        })
+    }
+    finally{}
+
+}
+
+run().catch(console.dir)
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
